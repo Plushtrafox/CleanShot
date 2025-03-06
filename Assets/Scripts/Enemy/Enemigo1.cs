59e7e7b263541ed9197e3ba6e297b750dd88d208
Plush187 <plushtrafoxelpirate@gmail.com>
2025-03-06 18:06:46 -0500

    Movimiento Practico del Enemigo
    Se una un script al cual se le pueden añadir animaciones si es un assets que las disponga

---
+++ Assets/Scripts/Enemy/Enemigo1.cs
@@ -0,0 +1,77 @@
+using System.Numerics;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Enemigo1 : MonoBehaviour
+{
+    public int rutina;
+    public float cronometro;
+    public Animator ani;
+    public UnityEngine.Quaternion angulo;
+    public float grado;
+
+    GameObject target;
+    public bool ataque;
+
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        target = GameObject.Find("Player");
+    }
+
+    public void Comportamiento_Enemigo()
+    {
+        if (UnityEngine.Vector3.Distance(transform.position, target.transform.position) > 5)
+        {
+
+            cronometro += 1 * Time.deltaTime;
+            if (cronometro >= 4)
+            {
+                rutina = UnityEngine.Random.Range(0, 5);
+                cronometro = 0;
+            }
+            switch (rutina)
+            {
+                
+
+                case 0:
+                    grado = UnityEngine.Random.Range(0, 360);
+                    angulo = UnityEngine.Quaternion.Euler(0, grado, 0);
+                    rutina++;
+                    break;
+
+                case 1:
+                    transform.rotation = UnityEngine.Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+                    transform.Translate(UnityEngine.Vector3.forward * 1 * Time.deltaTime);
+                    
+                    break;
+            }
+        }
+        else
+        {
+
+            if (UnityEngine.Vector3.Distance(transform.position, target.transform.position) > 1 && !ataque)
+            {
+                var lookpos = target.transform.position - transform.position;
+                lookpos.y = 0;
+                var rotation = UnityEngine.Quaternion.LookRotation(lookpos);
+                transform.rotation = UnityEngine.Quaternion.RotateTowards(transform.rotation, rotation, 4);
+               
+                transform.Translate(UnityEngine.Vector3.forward * 3 * Time.deltaTime);
+            }
+            else
+            {
+                ataque = true;
+            }
+        }
+    }
+
+    public void final_Ani()
+    {
+        ataque = false;
+    }    
+    private void Update()
+    {
+        Comportamiento_Enemigo();
+    }
+}
